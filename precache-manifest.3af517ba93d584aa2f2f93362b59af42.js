9b6c2464979479a151995b3f9ee9af47867a4225
Daniel Munoz <daniel.munozdev@gmail.com>
2019-05-13 21:59:14 -0700

    Updates

---
+++ precache-manifest.3af517ba93d584aa2f2f93362b59af42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52d47861a81103e839e93a2bb95d710",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "70153c812049f8cbf74a",
+    "url": "/burger/static/css/main.b60d562a.chunk.css"
+  },
+  {
+    "revision": "65f92cdda3ff678366e8",
+    "url": "/burger/static/js/2.e6e96d4f.chunk.js"
+  },
+  {
+    "revision": "70153c812049f8cbf74a",
+    "url": "/burger/static/js/main.b6b25d0e.chunk.js"
+  },
+  {
+    "revision": "5ae864a761ffe3b0b200",
+    "url": "/burger/static/js/runtime~main.a953f988.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
